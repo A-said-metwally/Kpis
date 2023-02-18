6857ecb1eda0e579e726e05979a31670d76ff11f
Ahmed Said <86842879+A-said-metwally@users.noreply.github.com>
2023-02-18 12:37:06 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import head from '../utils/head'
+import Head from '../utils/head'
 import '../styles/globals.css'
 
 
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   
   return (
       <div className=''>
-        <head title="Al Watania Poultry"/>
+        <Head title="Al Watania Poultry"/>
         <Component {...pageProps}   />    
       </div>
   )
